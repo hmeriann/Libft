4713d794891cf9c55801bbf963152f57d9d81820
Heidi Merianne <hmeriann@va-a5.kzn.21-school.ru>
2021-04-29 17:41:35 +0300

    memcmp fixed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hmeriann <hmeriann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 17:06:32 by hmeriann          #+#    #+#             */
-/*   Updated: 2021/04/27 14:11:25 by hmeriann         ###   ########.fr       */
+/*   Updated: 2021/04/29 15:53:03 by hmeriann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned const char	*tmp_s1;
-	unsigned const char	*tmp_s2;
+	size_t			i;
+	unsigned char	*tmp_s1;
+	unsigned char	*tmp_s2;
 
+	i = 0;
+	if (n == 0)
+		return (0);
 	tmp_s1 = (unsigned char *)s1;
 	tmp_s2 = (unsigned char *)s2;
-	while (n-- > 0 && *tmp_s1 == *tmp_s2)
-		tmp_s1++, tmp_s2++;
-	return (*tmp_s1 - *tmp_s2);
+	while (i < (n - 1) && (tmp_s1[i] == tmp_s2[i]))
+		i++;
+	return (tmp_s1[i] - tmp_s2[i]);
 }
