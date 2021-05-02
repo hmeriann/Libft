462b15f59e680df281f5282e1221fc495b41045e
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-02 22:34:38 +0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -14,17 +14,28 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*tmp;
+	char	*tmp;	
+	size_t	return_len;
+	size_t	s_len;
 
-	if (!s || !start || !len)
+	if (s == NULL)
 		return (NULL);
-	tmp = (char *)malloc(sizeof(char) * (len + 1));
-	if (!tmp)
-		return (NULL);
-	if ((unsigned int)ft_strlen(s) < start)
+	s_len = ft_strlen(s);
+	if (s_len < start)
 		return (ft_strdup(""));
-	if (ft_strlen(s + start) < len)
-		len = (size_t)ft_strlen(s + start);
-	ft_strlcpy(tmp, s + start, len + 1);
+	return_len = ft_min((s_len - start), len);
+	tmp = (char *)malloc(sizeof(char) * (return_len + 1));
+	if (tmp == NULL)
+		return (NULL);
+	ft_strlcpy(tmp, s + start, return_len + 1);
 	return (tmp);
 }
+
+// int main (void)
+// {
+// 	char *str = "i just want this part #############";
+// 	size_t size = 22;
+// 	char *ret = ft_substr(str, 0, size);
+
+// 	printf("%s\n", ret);
+// }
