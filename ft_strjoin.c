1f93967714da15452925abb9256918b6edde4d84
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-04 14:27:38 +0300

    strjoin - something with malloc protection in unit tests

---
+++ ft_strjoin.c
@@ -0,0 +1,44 @@
+# include "libft.h"
+# include <stdio.h>
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    char    *begin;
+    char    *end;
+    char    *total;
+    int     i;
+    int     j;
+
+    begin = (char *)s1;
+    end = (char *)s2;
+    total = (char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+    if (total == NULL)
+        return (NULL);
+    i = 0;
+    while (begin[i] != '\0')
+    {
+        total[i] = begin[i];
+        i++;
+    }
+    printf("%s\n", total);
+    j = 0;
+    while (end[j] != '\0')
+    {
+        total[i + j] = end[j];
+        j++;
+    }
+        printf("%s\n", total);
+    total[i + j] = '\0';
+        printf("%s\n", total);
+    return (total);
+}
+
+// int main(void)
+// {
+//     char    *s1 = "";
+//     char    *s2 = "";
+//     char    *ttl;
+
+//     ft_strjoin(s1, s2);
+//     return (0);
+// }
