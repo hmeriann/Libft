cf4773500487a78ba4e90394e56018eb5ff6bed6
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-07 23:40:38 +0300

    lstsize

---
+++ ft_lstsize.c
@@ -0,0 +1,17 @@
+# include "libft.h"
+/* считает количество элементов в листе. получает указатель на голову и возврашает количество элементов*/
+
+int ft_lstsize(t_list *lst)
+{
+	int counter;
+	t_list *tmp;
+
+	if (lst == NULL)
+		return (NULL);
+	while (tmp -> next != NULL)
+	{
+		tmp = tmp -> next;
+		counter++;
+	}
+	return (counter);
+}
