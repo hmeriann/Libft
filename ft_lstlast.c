5ce582a203246d373bd52688526aca772ba74f06
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-07 23:45:18 +0300

    lstlast

---
+++ ft_lstlast.c
@@ -0,0 +1,13 @@
+# include "libft.h"
+/* передвигает начало списка вниз по списку и находит последний элемент списка*/
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*tmp;
+
+	if (lst == NULL)
+		return (NULL);
+	tmp = lst;
+	while (tmp -> next)
+		tmp = tmp -> next;
+	return(tmp);
+}
