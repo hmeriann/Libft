7fdf19fb77bbd648bd159b3d3e51862d833c0995
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-07 20:45:52 +0300

    protected putendl and putstr

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -17,8 +17,12 @@ void	ft_putstr_fd(char *s, int fd)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0') {
-		write(fd, &s[i], 1);
-			i++;
+	if (s)
+	{
+		while (s[i] != '\0') 
+		{
+			write(fd, &s[i], 1);
+				i++;
+		}
 	}
 }
