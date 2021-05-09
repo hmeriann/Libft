10e53e3fa311bc985185536fe2c3bef429232ac7
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-09 18:59:08 +0300

    lst add back - OK

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,34 +6,37 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
     t_list	*last;
 
+	if (new == NULL)
+		return ;
 	if (*lst == NULL)
-		lst = &new;
+		*lst = new;
 	else
 	{
 		last = ft_lstlast(*lst);
-		new -> next = NULL;
+		// new -> next = NULL;
 		last -> next = new;
 	}
 }
 
-t_list	*make_test_item()
-{
-	t_list	*l = ft_lstnew(ft_strdup("nyacat"));
-	return (l);
-}
+// t_list	*make_test_item()
+// {
+// 	t_list	*l = ft_lstnew(ft_strdup("nyacat"));
+// 	return (l);
+// }
+
+// int main()
+// {
+// 	t_list *l1 = NULL;
+// 	t_list *l_new = ft_lstnew(ft_strdup("OK"));
+// 	// t_list *l_new = NULL;
 
 
-int main()
-{
-	t_list *l1 = make_test_item();
-	t_list *l_new = ft_lstnew(ft_strdup("OK"));
+// 	ft_lstadd_back(&l1, l_new);
+// 	while (l1 != NULL)
+// 	{
+// 		printf("%s -> ", (char *)(l1 -> content));
+// 		l1 = l1 -> next;
+// 	}
 
-	ft_lstadd_back(&l1, l_new);
-	while (l1 != NULL)
-	{
-		printf("%s -> ", (char *)(l1 -> content));
-		l1 = l1 -> next;
-	}
-
-	return (0);
-}
+// 	return (0);
+// }
