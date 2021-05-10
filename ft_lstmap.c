7c38f2109ee4a79c28bafdccc0beb714d4a32084
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-10 16:28:07 +0300

    lstmap OK

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include <stdio.h>
 /*шагает по листу и применяет функцию f к каждому элементу списка. Из результата применения функции создает новый список. Применяет функцию del, если необходимо*/
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
@@ -6,7 +7,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*first_element;
 	t_list	*new_lst;
 
-	if (!f || !del)
+	if (!f)
 		return (NULL);
 	first_element = NULL;
 	while (lst)
@@ -29,3 +30,35 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (first_element);
 }
+
+// t_list	*make_test_list()
+// {
+// 	t_list	*l1 = ft_lstnew(ft_strdup("one"));
+// 	t_list	*l2 = ft_lstnew(ft_strdup("two"));
+// 	t_list	*l3 = ft_lstnew(ft_strdup("three"));
+// 	l1 -> next = l2;
+// 	l2 -> next = l3;
+// 	return (l1);
+// }
+
+// void *f(void *content)
+// {
+// 	printf("%s\n", content);
+// 	content = (void *)strdup("OK");
+// 	return (content);
+// }
+
+// int main()
+// {
+// 	t_list *result;
+// 	t_list 	*l1 = make_test_list();
+// 	result = ft_lstmap(l1, f, NULL);	
+
+// 	while (result != NULL)
+// 	{
+// 		printf("%s -> ", (char *)(result -> content));
+// 		result = result -> next;
+// 	}
+
+// 	return (0);
+// }
