d950bf218f1415eee0682b3d06a609bca202db57
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-11 01:58:41 +0300

    ft_split        : 1.MOK 2.KO 3.MOK 4.KO 5.MKO 6.OK LEAKS.KO [0x7f87294059c0 : 1] 7.OK 8.OK 9.OK LEAKS.KO [0x7f8729405990 : 1] 10.OK 11.MOK 12.OK 13.OK 14.MOK 15.OK 16.MOK 17.OK 18.OK 19.MOK 20.OK 21.OK 22.MOK 23.KO 24.OK LEAKS.KO [0x7f87294059d0 : 1] 25.MOK 26.KO 27.MOK 28.OK LEAKS.KO [0x7f87294059e0 : 1]

--- ft_split.c
+++ ft_split.c
@@ -1,21 +1,22 @@
 # include "libft.h"
 # include <stdio.h>
 
-static void array_free(char **res_array, size_t line_num);
+static char **array_free(char **res_array);
 static int  words_counter(const char *s, int l, char c);
+char	*add_str_in_array(char const *s, unsigned int start, size_t len);
 
-static void array_free(char **res_array, size_t line_num)
+static char **array_free(char **res_array)
 {
     size_t i;
 
     i = 0;
-    while (line_num >= i)
+    while (res_array[i])
     {
         free(res_array[i]);
         i++;
     }
     free(res_array);
-    res_array = NULL;    
+    return (NULL);    
 }
 
 static int  words_counter(const char *s, int l, char c)
@@ -39,80 +40,65 @@ static int  words_counter(const char *s, int l, char c)
     return (words_count);
 }
 
-
-
 char    **ft_split(char const *s, char c)
 {
     size_t left;
     size_t right;
     size_t words_count;
     size_t line_num;
-    char **res_array;//указывает на первый элемент массива указателей, останется на месте, чтобы вернуть в конце функции
+    char **res_array;
     
     if (s == NULL)
         return (NULL);
 
-// считаем количество слов/строк
     left = 0; 
     words_count = words_counter(s, left, c);
+        // printf("wcount = %zu, separator = '%c'\n", words_count, c);
 
-//выделить память под массив указателей на начало строк
     res_array = (char **)malloc((words_count + 1) * sizeof(char *));
     if (res_array == NULL)
         return (NULL);
-    printf("words = %zu, res_array = %p\n", words_count, res_array);
-//двигаемся по основной строке и выделяем подстроки
-    left = 0; //индекс первого символа в слове
-    right = 0; //индекс последнего символа в слове - его двигаем
+
+    left = 0;
+    right = 0;
     line_num = 0;
 
-    while (s[right] != '\0') //пока не закончилась строка
+    while (s[right] != '\0')
     {
-        while (s[right] != c && s[right] != '\0') // пока не совпадает со стоп-символом и не дошли до конца строки
-            right++; // листаем (до конца слова или до разделителя)
-//добавить очередную найденную строку в возвращаемый массив
-        
-        res_array[line_num] = (char *)malloc((right - left + 1) * (sizeof(char))); // выделяем под эту длину + символ память
-        if (res_array[line_num] == NULL) // если не получилось
-            array_free(res_array, line_num);
-        if (right - left > 0 && res_array[line_num] != NULL) // если слово имеет длину
+        while (s[right] != c && s[right] != '\0')
+            right++;
+        res_array[line_num] = (char *)malloc((right - left + 1) * (sizeof(char)));
+        if (res_array[line_num] == NULL)
+            return (array_free(res_array));
+        if (right - left > 0)
         {
-            printf("res_array[line_num] = %s\n", res_array[line_num]);
-            res_array[line_num] = ft_substr(s, left, right - left);
-            printf("res_array[line_num] after allocation= %s\n", res_array[line_num]);
-            line_num++; //перешли к следующему слову
+            while (s[left] != c && s[left] != '\0')
+            {
+                res_array[line_num][left] = s[left];
+                    left++;
+            }
+            res_array[line_num][right] = '\0';
+            line_num++;
         }
-
-
-//переходим к следующему слову (подстроке)
-        right++;
-        left = right;
+        // left = right;
+        
+        // printf("res array = %s\n", res_array[line_num]);
+        while (s[right] == c && s[right] != '\0')
+            right++;    
     }
+    res_array[line_num] = NULL;
     return (res_array);
 }
 
 
-
-// int main()
+// int main(void)
 // {
-//     char *str = " Hello Im seacrching mem leak   tetst test  ";
-//     char **arr;
-//     arr = ft_split(str, ' ');
-//     int i = 0;
-//     while(arr[i])
-//     {
-//         printf("%s\n", arr[i++]);
-//             }
-//     sleep(100000);
-// } 
-int main(void)
-{
-	char *string = "split";
-    char c = ' ';
-    char ** result;
+// 	char *string = "split ";
+//     char c = ' ';
+//     char ** result;
     
-    result = ft_split(string, c);
-    for (int i = 0; i < 5; i++)
-        printf("%s\n", result[i]);
-    return (0);
-}
+//     result = ft_split(string, c);
+//     for (int i = 0; i < 5; i++)
+//         printf("in main = %s\n", result[i]);
+//     return (0);
+// }
