76e38510d13d7bba4a369e6737532bfd39733654
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-05-11 20:17:57 +0300

    split - OK

--- ft_split.c
+++ ft_split.c
@@ -1,93 +1,96 @@
 # include "libft.h"
 # include <stdio.h>
 
-static char **array_free(char **res_array);
-static int  words_counter(const char *s, int l, char c);
-char	*add_str_in_array(char const *s, unsigned int start, size_t len);
+static char		**array_free(char **res_array);
+static size_t	words_counter(const char *s, size_t l, char c);
+static	void	action_with_str(const char *s, char **res_array, char c);
+
 
 static char **array_free(char **res_array)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (res_array[i])
-    {
-        free(res_array[i]);
-        i++;
-    }
-    free(res_array);
-    return (NULL);    
+	i = 0;
+	while (res_array[i])
+	{
+		free(res_array[i]);
+		res_array[i] = NULL;
+		i++;
+	}
+	free(res_array);
+	res_array = NULL;
+	return (NULL);
 }
 
-static int  words_counter(const char *s, int l, char c)
+static size_t  words_counter(const char *s, size_t l, char c)
 {
-    int words_count;
-    int state;
+	size_t words_count;
+	int state;
 
-    words_count = 0;
-    state = 0;
-    while (s[l] != '\0')
-    {
-        if (s[l] == c)
-            state = 0;
-        else if (state == 0) 
-            {
-                state = 1;
-                words_count++;
-            }
-        l++; 
-    }
-    return (words_count);
+	words_count = 0;
+	state = 0;
+	while (s[l] != '\0')
+	{
+		if (s[l] == c)
+			state = 0;
+		else if (state == 0) 
+			{
+				state = 1;
+				words_count++;
+			}
+		l++; 
+	}
+	return (words_count);
+}
+
+static void	action_with_str(const char *s, char **res_array, char c)
+{
+	size_t left;
+	size_t right;
+	size_t line_num;
+
+	line_num = 0;
+	right = 0;
+	left = 0;
+	while (s[right] != '\0')
+	{
+		while (s[right] == c && s[right] != '\0')
+			right++;
+		while (s[right] != c && s[right] != '\0')
+		{
+			right++;
+			left++;
+		}
+		if (left > 0)
+		{
+			res_array[line_num] = ft_substr(s, right - left, left);
+			if (res_array[line_num] == NULL)
+			{
+				array_free(res_array);
+				return ;
+			}
+			line_num++;
+			left = 0;
+		}
+	}
+	res_array[line_num] = NULL;
 }
 
 char    **ft_split(char const *s, char c)
 {
-    size_t left;
-    size_t right;
-    size_t words_count;
-    size_t line_num;
-    char **res_array;
-    
-    if (s == NULL)
-        return (NULL);
-
-    left = 0; 
-    words_count = words_counter(s, left, c);
-        // printf("wcount = %zu, separator = '%c'\n", words_count, c);
-
-    res_array = (char **)malloc((words_count + 1) * sizeof(char *));
-    if (res_array == NULL)
-        return (NULL);
-
-    left = 0;
-    right = 0;
-    line_num = 0;
-
-    while (s[right] != '\0')
-    {
-        while (s[right] != c && s[right] != '\0')
-            right++;
-        res_array[line_num] = (char *)malloc((right - left + 1) * (sizeof(char)));
-        if (res_array[line_num] == NULL)
-            return (array_free(res_array));
-        if (right - left > 0)
-        {
-            while (s[left] != c && s[left] != '\0')
-            {
-                res_array[line_num][left] = s[left];
-                    left++;
-            }
-            res_array[line_num][right] = '\0';
-            line_num++;
-        }
-        // left = right;
-        
-        // printf("res array = %s\n", res_array[line_num]);
-        while (s[right] == c && s[right] != '\0')
-            right++;    
-    }
-    res_array[line_num] = NULL;
-    return (res_array);
+	size_t left;
+	size_t words_count;
+	char **res_array;
+	
+	if (s == NULL)
+		return (NULL);
+	left = 0; 
+	words_count = words_counter(s, left, c);
+	res_array = (char **)malloc((words_count + 1) * sizeof(char *));
+	if (res_array == NULL)
+		return (NULL);
+	action_with_str(s, res_array, c);
+	return (res_array);
 }
 
 
@@ -96,7 +99,7 @@ char    **ft_split(char const *s, char c)
 // 	char *string = "split ";
 //     char c = ' ';
 //     char ** result;
-    
+	
 //     result = ft_split(string, c);
 //     for (int i = 0; i < 5; i++)
 //         printf("in main = %s\n", result[i]);
