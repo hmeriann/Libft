22250557565e2d4c62fb2971a5c711b56682f44a
Heidi Merianne <hmeriann@va-f4.kzn.21-school.ru>
2021-05-12 19:48:28 +0300

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,35 +6,45 @@
 /*   By: hmeriann <hmeriann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 10:56:32 by hmeriann          #+#    #+#             */
-/*   Updated: 2021/05/12 17:12:21 by hmeriann         ###   ########.fr       */
+/*   Updated: 2021/05/12 19:47:07 by hmeriann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	max_min_handling(long long res, int sign)
+{
+	if (res > 2147483647)
+	{
+		if (sign == 0)
+			return (-1);
+		return (0);
+	}
+	return (1);
+}
+
 int		ft_atoi(const char *str)
 {
 	int	i;
-	int	minusCounter;
-	int	res;
+	int	sign;
+	long long	res;
 
 	i = 0;
-	minusCounter = 0;
+	sign = 0;
 	res = 0;
 	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
 		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			minusCounter++;
-		i++;
-	}
+	if ((str[i] == '-' || str[i] == '+') && ft_isdigit(str[i + 1]))
+		if (str[i++] == '-')
+			sign = 1;
 	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
+		if (max_min_handling(res, sign) != 1)
+			return (max_min_handling(res, sign));
 		res = 10 * res + (str[i] - '0');
 		i++;
 	}
-	if (minusCounter % 2 == 1)
+	if (sign == 1)
 		res = -res;
 	return (res);
 }
